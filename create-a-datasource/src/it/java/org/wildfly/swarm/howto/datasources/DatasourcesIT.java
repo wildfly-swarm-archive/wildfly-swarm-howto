1e6f60f9a351968c5109f94ef71c9f7d62feaf8c
Ken Finnigan <ken@kenfinnigan.me>
2017-02-01 13:43:37 -0500

    Various updates: (#1)
    - Use `project-defaults.yml` instead of deprecated `project-stages.yml`
     - Use latest SNAPSHOT
     - Add arquillian.xml for Drone testing to set PhantomJS Driver
     - Change ProjectOdd to Sonatype for repositories

--- create-a-datasource/src/it/java/org/wildfly/swarm/howto/datasources/DatasourcesIT.java
+++ create-a-datasource/src/it/java/org/wildfly/swarm/howto/datasources/DatasourcesIT.java
@@ -4,12 +4,14 @@ import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.*;
 
 /**
  * @author Bob McWhirter
+ * @author Ken Finnigan
  */
 @RunWith(Arquillian.class)
 public class DatasourcesIT {
@@ -20,6 +22,6 @@ public class DatasourcesIT {
     @Test
     public void testIt() throws Exception {
         browser.navigate().to("http://localhost:8080/");
-        assertEquals("Found the datasource", browser.getPageSource().trim());
+        assertEquals("Found the datasource", browser.findElement(By.tagName("body")).getText());
     }
 }
